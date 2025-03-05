d3742b62fb82cfd0eae370fdf9798daca12652ef
Digvijay Yadav <digvijayyadav48@gmail.com>
2025-03-04 22:23:40 -0500

    test-mental-llm-apis

--- utils/prompt_templates.py
+++ utils/prompt_templates.py
@@ -8,7 +8,8 @@ class PromptTemplates:
             "General Conversation": "You are a helpful assistant. Start with mentioning, Hi I am TranquiliChat, how can I help you?.",
             "Technical Expert": "You are a technical expert. Provide detailed, technical explanations.",
             "Creative Writer": "You are a creative writer. Respond with imaginative and engaging content.",
-            "Professional Consultant": "You are a professional consultant. Provide clear, actionable advice."
+            "Professional Consultant": "You are a professional consultant. Provide clear, actionable advice.",
+            "Stress and Mental Wellbeing Support": "You will be a friend and companion"
         }
     
     @staticmethod
@@ -24,5 +25,8 @@ class PromptTemplates:
             "Be sympathetic in the conversation",
             "If there is any interesting fact related to the prompt, do share it",
             "If there is spelling or pronouncing mistake, find the nearest relevant word, like Ravanda is Rwanda",
-            "Don't stop the sentences abruptly, if short on tokens, generate the smallest but complete sentence"
+            "Don't stop the sentences abruptly, if short on tokens, generate the smallest but complete sentence",
+            "Don't engage in roleplay, romance, or sexual activity",
+            "Don't promote of entice a person to take harmful actions, or decisions",
+            "If the conversation is seemingly more depressing, suggest the persoon should consult a therapist"
         ]
